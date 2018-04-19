baaa929fbbcc99d9ce754235dfa428ecfdb8bdef
Sebastian Sabadus <sabadus_sebastian@yahoo.com>
2018-04-19 22:37:37 +0300

    Pasword validators added

--- src/superValidators.ts
+++ src/superValidators.ts
@@ -26,6 +26,54 @@ export class SuperValidators {
       return !isNaN(value) && value > max ? {'max': max, 'actualValue': control.value} : null;
     };
   }
+  public static hasNumber(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const error: ValidationErrors = { hasNumber: false };
+
+      if (!control.value || !/\d/.test(control.value)) {
+        return error;
+      }
+
+      return null;
+    };
+  }
+
+  public static hasUpper(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const error: ValidationErrors = { hasUpper: false };
+
+      if (!control.value || control.value.search(/[A-Z]/) < 0) {
+        return error;
+      }
+
+      return null;
+    };
+  }
+
+  public static hasLower(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const error: ValidationErrors = { hasLower: false };
+
+      if (!control.value || control.value.search(/[a-z]/) < 0) {
+        return error;
+      }
+
+      return null;
+    };
+  }
+
+  public static hasSpecial(): ValidatorFn {
+      return (control: AbstractControl): ValidationErrors | null => {
+        const error: ValidationErrors = { hasSpecial: false };
+        const format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+
+        if (!control.value || !format.test(control.value)) {
+          return error;
+        }
+
+        return null;
+      };
+   }
 }
 
 function isEmptyInputValue(value: any): boolean {
