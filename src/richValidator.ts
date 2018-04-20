f9474ce4a87c9020702a32f60cf4f97d80d260d6
Sebastian <sabadus_sebastian@yahoo.com>
2018-04-20 16:07:16 +0300

    richValidator: between

--- src/richValidator.ts
+++ src/richValidator.ts
@@ -8,4 +8,16 @@ export class RichValidator {
   public static base64(str: string): string {
     return btoa(str);
   }
+
+  public static between(min: number, max: number, inclusive?: boolean): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      if (!control.value || !min || !max) {
+        return {'between': false}
+      }
+      if(inclusive){
+        return control.value >= min && control.value <= max ? null : {'between': false};
+      }
+      return control.value > min && control.value < max ?  null : {'between': false};
+    };
+  }
 }
